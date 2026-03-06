2ad395f6248d6634870948590b562c93411d8f17
yuzhiliu8 <yuzhiliu8@gmail.com>
2026-03-05 20:44:16 -0500

    add lazy git

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,22 @@
+-- ================================================================================================
+-- TITLE : lazygit.nvim
+-- ABOUT : lazygit integration for neovim
+-- ================================================================================================
+
+return {
+	"kdheepak/lazygit.nvim",
+	lazy = true,
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	keys = {
+		{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+	},
+}
